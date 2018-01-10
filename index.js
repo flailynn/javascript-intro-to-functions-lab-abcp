0a867fcbb1d236c2058ba820024966c9b3bee01e
Anita Beauchamp <amadamspr@gmail.com>
2018-01-10 05:04:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if(string === string.toUppwerCase()) {
+  } else if(string === string.toUpperCase()) {
     return "YES INDEED!";
   } else if(srting == "I love you, Grandma") {
     return "I love you, too.";
